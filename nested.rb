33e52bf7219e856575bbbf4171ed49735fb2ec6d
Stephen Wright <scw248@gmail.com>
2019-01-05 23:44:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,12 +81,17 @@ def adding_matz
           :languages => ["C"]
         }
      }
-end
-
-programmer_hash[:yukihiro_matsumoto] = {
+  
+  programmer_hash[:yukihiro_matsumoto] = {
   :known_for => "Ruby",
   :languages => ["LISP", "C"]
 }
+
+programmer_hash
+     
+end
+
+
 
 def changing_alan
 	programmer_hash = 
